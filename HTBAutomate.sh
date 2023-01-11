cf3db4ff17efb7dbd5ed2d5470a4e9aa0d7e822e
TheGex777 <80421683+TheGex777@users.noreply.github.com>
2023-01-11 18:59:01 +0000

    Update HTBAutomate.sh

--- HTBAutomate.sh
+++ HTBAutomate.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
-read -p "Enter the name of the box: " reply
-mkdir $reply;
-echo "Parent directory and subdirectories created."
+main() {
+  box_name=$1
+  ip_address=$2
 
-make_directories(){
-	cd $reply && mkdir Enumeration Exploits Flags Notes Screenshots Scripts;
-	cd Enumeration && mkdir WebContent RPC SMB DNS LDAP FTP Usernames-and-Passwords SystemInfo Nmap;
-	cd ..;
+  mkdir -p "$box_name/Enumeration/WebContent" "$box_name/Enumeration/RPC" "$box_name/Enumeration/SMB" "$box_name/Enumeration/DNS" "$box_name/Enumeration/LDAP" "$box_name/Enumeration/FTP" "$box_name/Enumeration/Usernames-and-Passwords" "$box_name/Enumeration/SystemInfo" "$box_name/Enumeration/Nmap" "$box_name/Exploits" "$box_name/Flags" "$box_name/Notes" "$box_name/Screenshots" "$box_name/Scripts"
+  echo "Parent directory and subdirectories created."
+
+  if prompt_yes_no "Do you want to download external scripts for automation (e.g linpeas, nmapAutomator)? (y/n): "; then
+    echo "Downloading..." 
+    download_scripts
+    echo "Scripts downloaded and are in the Scripts subdirectory."
+  fi
+  
+  if prompt_yes_no "Do you want to run nmapAutomator script on the machine? (y/n): "; then
+    if [[ -z $ip_address ]]; then
+      ip_address=$(prompt_input "Enter the IP Address of the machine: ")
+    fi
+    bash "$box_name/Scripts/nmapAutomator.sh" $ip_address All
+  fi
 }
 
-make_directories
+prompt_yes_no() {
+  read -p "$1" yn
+  case $yn in
+    [Yy]* ) return 0;;
+    [Nn]* ) return 1;;
+    * ) echo "Please answer yes or no."; prompt_yes_no "$1";;
+  esac
+}
 
-while true; do
-    read -p "Do you want to download external scripts for automation (e.g linpeas, nmapAutomator)? (y/n): " yn
-    case $yn in
-        [Yy]* ) echo "Downloading..." 
-        wget -q https://raw.githubusercontent.com/carlospolop/PEASS-ng/master/linPEAS/linpeas.sh
-        wget -q https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh
-	wget -q https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh
-	wget -q https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/master/lse.sh
-	wget -q https://raw.githubusercontent.com/carlospolop/PEASS-ng/master/winPEAS/winPEASbat/winPEAS.bat
-	wget -q https://raw.githubusercontent.com/21y4d/nmapAutomator/master/nmapAutomator.sh
-	mv *.sh *.bat Scripts && cd Scripts && chmod +x nmapAutomator.sh; 
-	echo "Scripts downloaded and are in the Scripts subdirectory."
-	break;;	
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+prompt_input() {
+  read -p "$1" input
+  if [[ -z $input ]]; then
+    echo "Input is required."; prompt_input "$1"
+  else
+    echo "$input"
 
-while true; do
-    read -p "Do you want to run nmapAutomator script on the machine? (y/n): " yn
-    case $yn in
-        [Yy]* ) read -p "Enter the IP Address of the machine. : " IP;
-        bash nmapAutomator.sh $IP All; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
