6640976cc9a2e4847c5f15a792cc76b653dc741c
TheGex777 <80421683+TheGex777@users.noreply.github.com>
2023-12-11 16:00:56 +0000

    Update kali_setup.sh

--- kali_setup.sh
+++ kali_setup.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+!/bin/bash
+
 
 #List of packages
 packages=(
 docker
 docker-compose
 bloodhound
-bloodhound.py
+bloodhound-python
 neo4j
 nishang
 terminator
@@ -31,6 +32,10 @@ sslscan
 whatweb
 python3-pip
 tnscmd10g
+jq
+ldap-utils
+ffuf
+python3-certipy
 )
 
 #Install the packages
@@ -51,15 +56,40 @@ SUB_DIRe="Windows_Tools"
 mkdir ${MAIN_DIR}
 mkdir -p ${MAIN_DIR}/${SUB_DIRa} ${MAIN_DIR}/${SUB_DIRb} ${MAIN_DIR}/${SUB_DIRc} ${MAIN_DIR}/${SUB_DIRd} ${MAIN_DIR}/${SUB_DIRe}
 
+#Install Sliver
+curl https://sliver.sh/install|sudo bash
 
 #Switch to the Tools directory
 cd ${MAIN_DIR}
-#Grab external recon tools with git or wget
+
+#Download External Tools
+pip3 install git-dumper
+sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git
 git clone https://github.com/21y4d/nmapAutomator.git
-wget https://github.com/carlospolop/PEASS-ng/releases/download/20230702-bc7ce3ac/linpeas.sh
+git clone https://github.com/Kevin-Robertson/Powermad
+wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1
 wget https://github.com/carlospolop/PEASS-ng/releases/download/20230702-bc7ce3ac/winPEASany.exe
+wget https://raw.githubusercontent.com/carlospolop/PEASS-ng/master/winPEAS/winPEASps1/winPEAS.ps1
+wget https://github.com/swisskyrepo/SharpLAPS/releases/download/latest-SharpLAPS/SharpLAPS.exe
+git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries.git
+wget https://github.com/itm4n/PrintSpoofer/releases/download/v1.0/PrintSpoofer64.exe
+wget https://download.sysinternals.com/files/SysinternalsSuite.zip
+git clone https://github.com/dirkjanm/krbrelayx
+git clone https://github.com/micahvandeusen/gMSADumper
+git clone https://github.com/p0dalirius/pyLAPS
+git clone https://github.com/Greenwolf/ntlm_theft
+wget https://github.com/carlospolop/PEASS-ng/releases/download/20230702-bc7ce3ac/linpeas.sh
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32
+wget https://github.com/icsharpcode/AvaloniaILSpy/releases/download/v7.2-rc/Linux.x64.Release.zip
 wget https://raw.githubusercontent.com/TheGex777/OSCP-Resources/main/recon_template.ctb
+
+
+#Move Downloaded tools to proper directories
+unzip Linux.x64.Release.zip
+unzip ILSpy-linux-x64-Release.zip
+mv *.exe *.ps1 SysinternalsSuite.zip ${SUB_DIRe}
+mv Ghostpack-CompiledBinaries krbrelayx gMSADumper pyLAPS ntlm_theft ${SUB_DIRe}
+mv artifacts ${SUB_DIRd}
+mv linpeas.sh pspy32 pspy64 ${SUB_DIRd}
 sudo cp nmapAutomator/nmapAutomator.sh /bin
-sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git
