bb1a89e7b2e0a0576e3f959fbd815d45baca965e
TheGex777 <80421683+TheGex777@users.noreply.github.com>
2025-05-29 16:12:24 +0000

    Update kali_setup2.sh

--- kali_setup2.sh
+++ kali_setup2.sh
@@ -7,8 +7,14 @@ SUB_DIRa="Exploits"
 SUB_DIRb="Screenshots"
 SUB_DIRc="Notes"
 
-mkdir -p "$MAIN_DIR/$SUB_DIRa"
-mkdir -p "$MAIN_DIR/$SUB_DIRb"
-mkdir -p "$MAIN_DIR/$SUB_DIRc"
+mkdir -p ${MAIN_DIR}/${SUB_DIRa} ${MAIN_DIR}/${SUB_DIRb} ${MAIN_DIR}/${SUB_DIRc}
 
 echo "Directory '$MAIN_DIR' and its subdirectories created."
+
+read -p "Do you want to scan the host with autorecon and nmapAutomator? (Yes/No): " scan_choice
+
+if [[ "$scan_choice" =~ ^[Yy](es)?$ ]]; then
+	autorecon --only-scans-dir $MAIN_DIR
+else
+  echo "Scanning skipped."
+fi
